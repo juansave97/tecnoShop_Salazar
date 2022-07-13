0c95697653041e8b9afe5abba7b27c51f2327bfe
Julian Salazar <juansave97@gmail.com>
2022-07-13 18:15:56 -0500

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+
+import './Navbar.css';
+
+
+const Navbar = () => { 
+    return (
+        <nav>
+            <div className='Navbar'>
+                <div className='title'>
+                    TECNOSHOP
+                </div>
+                <div className='boton'>
+                    <button>Productos</button>
+                    <button>Nosotros</button>
+                    <button>Trabaja con nosotros</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
